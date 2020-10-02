03d1f28e22a648f81eb160fb32bea1d53926dd06
Akash Agrawal <akagr@outlook.com>
2020-10-02 16:40:03 +0530

    Themed org mode

--- dot-emacs-dot-d/modules/custom-file.el
+++ dot-emacs-dot-d/modules/custom-file.el
@@ -7,7 +7,7 @@
    '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "197cefea731181f7be51e9d498b29fb44b51be33484b17416b9855a2c4243cb1" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
  '(fira-code-mode-disabled-ligatures '("[]" "#{" "#(" "#_" "#_(" "x"))
  '(package-selected-packages
-   '(zenburn-theme counsel-projectile figlet flycheck-inline flycheck company-lsp lsp-ivy lsp-ui deadgrep ivy-rich magit-libgit libgit which-key rainbow-delimiters all-the-icons-dired dired-sidebar evil-commentary doom-modeline powerline doom-themes ripgrep company-capf yaml-mode flx evil-magit treemacs-all-the-icons treemacs-projectile treemacs-evil treemacs all-the-icons neotree elixir-mode alchemist company company-mode helm-projectile fira-code-mode use-package magit evil))
+   '(org-bullets zenburn-theme counsel-projectile figlet flycheck-inline flycheck company-lsp lsp-ivy lsp-ui deadgrep ivy-rich magit-libgit libgit which-key rainbow-delimiters all-the-icons-dired dired-sidebar evil-commentary doom-modeline powerline doom-themes ripgrep company-capf yaml-mode flx evil-magit treemacs-all-the-icons treemacs-projectile treemacs-evil treemacs all-the-icons neotree elixir-mode alchemist company company-mode helm-projectile fira-code-mode use-package magit evil))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
