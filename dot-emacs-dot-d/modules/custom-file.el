53dcdf26d61442cc5b45333481ae576367b98dee
Akash Agrawal <akagr@outlook.com>
2020-10-13 16:38:38 +0530

    Add support for typescript

--- dot-emacs-dot-d/modules/custom-file.el
+++ dot-emacs-dot-d/modules/custom-file.el
@@ -7,7 +7,7 @@
    '("8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "37a4701758378c93159ad6c7aceb19fd6fb523e044efe47f2116bc7398ce20c9" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "197cefea731181f7be51e9d498b29fb44b51be33484b17416b9855a2c4243cb1" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
  '(fira-code-mode-disabled-ligatures '("[]" "#{" "#(" "#_" "#_(" "x"))
  '(package-selected-packages
-   '(editorconfig ruby-end evil-org org-bullets zenburn-theme counsel-projectile figlet flycheck-inline flycheck company-lsp lsp-ivy lsp-ui deadgrep ivy-rich magit-libgit libgit which-key rainbow-delimiters all-the-icons-dired dired-sidebar evil-commentary doom-modeline powerline doom-themes ripgrep company-capf yaml-mode flx evil-magit treemacs-all-the-icons treemacs-projectile treemacs-evil treemacs all-the-icons neotree elixir-mode alchemist company company-mode helm-projectile fira-code-mode use-package magit evil))
+   '(evil-surround typescript-mode typescript editorconfig ruby-end evil-org org-bullets zenburn-theme counsel-projectile figlet flycheck-inline flycheck company-lsp lsp-ivy lsp-ui deadgrep ivy-rich magit-libgit libgit which-key rainbow-delimiters all-the-icons-dired dired-sidebar evil-commentary doom-modeline powerline doom-themes ripgrep company-capf yaml-mode flx evil-magit treemacs-all-the-icons treemacs-projectile treemacs-evil treemacs all-the-icons neotree elixir-mode alchemist company company-mode helm-projectile fira-code-mode use-package magit evil))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
