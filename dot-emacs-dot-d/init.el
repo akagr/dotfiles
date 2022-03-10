70b7ad24e670295ea4f860da7e4ba428fb165d3a
Akash Agrawal <akagr@outlook.com>
2022-03-10 15:46:26 +0530

    Remove references to ruby-end mode

--- dot-emacs-dot-d/init.el
+++ dot-emacs-dot-d/init.el
@@ -518,14 +518,6 @@
 (use-package elixir-mode
   :mode "\\.exs?\\'"
   :config
-  ;; Activate ruby end mode on elixir. This helps auto-inserting
-  ;; ruby style 'end' after writing 'do' in elixir.
-  (add-hook 'elixir-mode-hook
-            (lambda ()
-              (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                   "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-              (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-              (ruby-end-mode +1)))
   ;; Create a buffer-local hook to run elixir-format on save, only when we enable elixir-mode.
   (add-hook 'elixir-mode-hook
             (lambda () (add-hook 'before-save-hook 'elixir-format nil t))))
