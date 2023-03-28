de20d42937419548b59d32f3813eded759423887
Akash Agrawal <akagr@outlook.com>
2023-03-28 13:30:48 +0530

    Add fish config

---
+++ config.fish
@@ -0,0 +1,39 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#================================ Path ===============================
+
+# Adds homebrew and installed packages to the path
+if test -e /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+# Adds completions for asdf version manager to fish
+if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+end
+
+#========================== General Aliases ==========================
+alias du="dust -d 1"
+
+#=============================== DevOps ==============================
+alias k="kubectl"
+alias tf="terraform"
+
+# Adds completions for aws to fish
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
+# AWS uses identity center to manage SSO access using CLI
+# If SSO is not configured, run `aws configure sso` first.
+# Then, whenever a session expires or a new one is needed, run `aws sso login`.
+# That will generate and store temporary AWS credentials in cache.
+# `aws_load_credentials` is a helper to then load these credentials to standard
+# AWS environment variables like AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY etc.
+function aws_load_credentials -d "load aws credentials from sso cache file to environment variables"
+    if test (count $argv) != 1
+        echo "Example: aws_load_credentials <aws_profile_name>" >&2
+        return 1
+    end
+    eval $(aws configure export-credentials --profile $argv --format env)
+end
