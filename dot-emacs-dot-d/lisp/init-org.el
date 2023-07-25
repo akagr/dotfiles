85979b6eca9950c62555acbf0015fd9bd1b0418e
Akash Agrawal <akagr@outlook.com>
2023-07-25 12:07:59 +0530

    Automatically mark parent todos done when children are done

--- dot-emacs-dot-d/lisp/init-org.el
+++ dot-emacs-dot-d/lisp/init-org.el
@@ -137,4 +137,11 @@
                         'aa/config-tangle
                         nil 'make-it-local))))
 
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook #'org-summary-todo)
+
 (provide 'init-org)
