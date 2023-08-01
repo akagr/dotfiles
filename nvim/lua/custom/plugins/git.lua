2de08d91197dd88ae801c5f404811e1ac9255852
Akash Agrawal <akagr@outlook.com>
2023-08-01 19:05:03 +0530

    Add kickstart nvim configuration

---
+++ nvim/lua/custom/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+  "kdheepak/lazygit.nvim",
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require('telescope').load_extension('lazygit')
+  end,
+  keys = {
+    { "<leader>g", "<cmd>LazyGit<cr>", desc = "lazy[g]it" },
+  },
+}
---
+++ nvim/lua/custom/plugins/rooter.lua
@@ -0,0 +1,25 @@
+-- Lua
+return {
+  "ahmedkhalf/project.nvim",
+  lazy = false,
+  dependencies = {
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x' },
+  },
+  config = function()
+    require("project_nvim").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+    require('telescope').load_extension('projects')
+  end,
+  keys = {
+    {
+      "<leader>p",
+      function()
+        require 'telescope'.extensions.projects.projects {}
+      end,
+      desc = "[P]rojects",
+    },
+  },
+}
