8b8d3908f6610fe2927d77fde44f1c2f6b3a0a73
Akash Agrawal <akagr@outlook.com>
2024-03-04 12:51:11 +0530

    Switch to neogit
    Fugitive is closer to metal but neogit is way more familiar for a magit
    user (me).

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,13 +1,31 @@
 return {
-    'tpope/vim-fugitive',
-    dependencies = {
-      'folke/which-key.nvim',
-    },
-    config = function ()
-      local wk = require('which-key')
-
-      wk.register({
-        g = { ':Git<space>', '[g]it' },
-      }, { prefix = '<leader>', silent = false })
-    end
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+    "folke/which-key.nvim",
+  },
+  config = function()
+    require('neogit').setup({
+      disable_hint = true,
+      graph_style = "unicode",
+      signs = {
+        hunk = { "⏵", "⏷" },
+        item = { "⏵", "⏷" },
+        section = { "⏵", "⏷" },
+      },
+      integrations = {
+        telescope = true,
+        diffview = true,
+      }
+    })
+    local wk = require('which-key')
+    wk.register({
+      g = {
+        name = "+git",
+        s = { '<cmd>Neogit<cr>', '[s]tatus' },
+      }
+    }, { prefix = '<leader>', silent = false })
+  end,
 }
