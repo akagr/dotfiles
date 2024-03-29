6dfc0c076f7b7e66f6e39cd02e0d2c56def33ba0
Akash Agrawal <akagr@outlook.com>
2024-03-29 13:18:18 +0530

    Make oil more presentable

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,12 +1,32 @@
+local permission_hlgroups = {
+  ['-'] = 'NonText',
+  ['r'] = 'DiagnosticSignOk',
+  ['w'] = 'DiagnosticSignWarn',
+  ['x'] = 'DiagnosticSignError',
+}
+
 return {
   'stevearc/oil.nvim',
   opts = {
     columns = {
-      "icon",
-      "permissions",
-      "size",
-      "mtime",
-    }
+      {
+        "permissions",
+        highlight = function(permission_str)
+          local hls = {}
+          for i = 1, #permission_str do
+            local char = permission_str:sub(i, i)
+            table.insert(hls, { permission_hlgroups[char], i - 1, i })
+          end
+          return hls
+        end,
+      },
+      { 'size', highlight = 'Special' },
+      { 'mtime', highlight = 'Number' },
+      { "icon", add_padding = false },
+    },
+    win_options = {
+      winbar = "%{v:lua.require('oil').get_current_dir()}",
+    },
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
