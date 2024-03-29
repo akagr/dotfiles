22c909a0ff7d278a9320fe48f77df06614dbb9c8
Akash Agrawal <akagr@outlook.com>
2024-03-29 12:17:53 +0530

    Add rsi to neovim to get some readline keybindings

---
+++ nvim/lua/plugins/rsi.lua
@@ -0,0 +1,6 @@
+-- This adds some readline keybindings like C-f to go one character forward
+-- in insert mode, C-b to go back, C-e to go to end of line, etc.
+-- Coming from emacs, this is essential for me.
+return {
+  "tpope/vim-rsi"
+}
