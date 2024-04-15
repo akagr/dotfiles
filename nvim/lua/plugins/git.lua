186ad07f7a593163a6b17ec4fe9f55cf69583671
Akash Agrawal <akagr@outlook.com>
2024-04-15 20:13:56 +0530

    Add gitsigns

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,13 +1,23 @@
 return {
-  'tpope/vim-fugitive',
-  dependencies = {
-    'folke/which-key.nvim'
-  },
-  config = function()
-    local wk = require('which-key')
+  {
+    'tpope/vim-fugitive',
+    dependencies = {
+      'folke/which-key.nvim'
+    },
+    config = function()
+      local wk = require('which-key')
 
-    wk.register({
-      g = { ":Git ", "fu[git]ive"}
-    }, { prefix = '<leader>', silent = false })
-  end
+      wk.register({
+        g = { ":Git ", "fu[git]ive" }
+      }, { prefix = '<leader>', silent = false })
+    end
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        current_line_blame = true,
+      })
+    end
+  }
 }
