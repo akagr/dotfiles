24f83893994fee3f8bd17b72a242eb788a0f2c00
Akash Agrawal <akagr@outlook.com>
2024-04-15 23:03:53 +0530

    Add gitsigns as dependency and add keybindings

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,7 +2,40 @@ return {
   {
     'tpope/vim-fugitive',
     dependencies = {
-      'folke/which-key.nvim'
+      'folke/which-key.nvim',
+      {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+          require('gitsigns').setup({
+            current_line_blame = true,
+            on_attach = function(bufnr)
+              local gitsigns = require('gitsigns')
+
+              local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+              end
+
+              map('n', ']c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({ ']c', bang = true })
+                else
+                  gitsigns.nav_hunk('next')
+                end
+              end)
+
+              map('n', '[c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({ '[c', bang = true })
+                else
+                  gitsigns.nav_hunk('prev')
+                end
+              end)
+            end
+          })
+        end
+      }
     },
     config = function()
       local wk = require('which-key')
@@ -12,12 +45,4 @@ return {
       }, { prefix = '<leader>', silent = false })
     end
   },
-  {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup({
-        current_line_blame = true,
-      })
-    end
-  }
 }
