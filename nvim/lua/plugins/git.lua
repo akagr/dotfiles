1634569155c405cd8b955cf449a9b7a672befe3b
Akash Agrawal <akagr@outlook.com>
2024-04-15 17:58:29 +0530

    Replace neogit with fugitive

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,39 +1,13 @@
 return {
-  "NeogitOrg/neogit",
+  'tpope/vim-fugitive',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
-    "folke/which-key.nvim",
+    'folke/which-key.nvim'
   },
   config = function()
-    require('neogit').setup({
-      disable_hint = true,
-      graph_style = "unicode",
-      signs = {
-        hunk = { "⏵", "⏷" },
-        item = { "⏵", "⏷" },
-        section = { "⏵", "⏷" },
-      },
-      integrations = {
-        telescope = true,
-        diffview = true,
-      },
-      console_timeout = 15000,
-      mappings = {
-        popup = {
-          ["p"] = "PushPopup",
-          ["F"] = "PullPopup",
-          ["P"] = false
-        }
-      }
-    })
     local wk = require('which-key')
+
     wk.register({
-      g = {
-        name = "+git",
-        s = { '<cmd>Neogit<cr>', '[s]tatus' },
-      }
+      g = { ":Git ", "fu[git]ive"}
     }, { prefix = '<leader>', silent = false })
-  end,
+  end
 }
