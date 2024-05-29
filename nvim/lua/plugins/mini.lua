28740fae9b3060fc96251d18f6fa229e8c93a089
Akash Agrawal <akagr@outlook.com>
2024-05-29 12:35:17 +0530

    Replace mini with the original vim surround
    Not really using mini for anything else right now

--- nvim/lua/plugins/mini.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'echasnovski/mini.nvim',
-  version = '*',
-  config = function ()
-    require('mini.surround').setup()
-    require('mini.align').setup()
-  end
-}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,7 +5,8 @@ return {
     {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-    }
+    },
+    'nvim-lua/plenary.nvim',
   },
   config = function ()
     require('telescope').setup({
