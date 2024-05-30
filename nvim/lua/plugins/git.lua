959fdd8c10591b0b3071b33c2bd46a4ed4e788f2
Akash Agrawal <akagr@outlook.com>
2024-05-30 13:44:08 +0530

    Add lazygit as primary git plugin
    Still gonna keep fugitive for when I just want the command line

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,24 @@
 return {
   {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+  {
     'tpope/vim-fugitive',
     dependencies = {
       'folke/which-key.nvim',
@@ -37,12 +56,5 @@ return {
         end
       }
     },
-    config = function()
-      local wk = require('which-key')
-
-      wk.register({
-        g = { ":Git ", "fu[git]ive" }
-      }, { prefix = '<leader>', silent = false })
-    end
   },
 }
