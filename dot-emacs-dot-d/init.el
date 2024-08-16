f96bd1c26c94bf78688e7ef2e10c2499dde894ba
Akash Agrawal <akagr@outlook.com>
2024-08-16 23:59:16 +0530

    Add markdown

--- dot-emacs-dot-d/init.el
+++ dot-emacs-dot-d/init.el
@@ -32,8 +32,21 @@
 (require 'feat-yaml)
 (require 'feat-dockerfile)
 (require 'feat-typescript)
+(require 'feat-markdown)
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
 ;; Increase the amount of data which Emacs reads from the process
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(edit-indirect eglot)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
