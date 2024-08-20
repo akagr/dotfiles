f5f7a3d549a597343a0b6179fa92856c648b4c46
Akash Agrawal <akagr@outlook.com>
2024-08-20 14:02:04 +0530

    Fix black color for vterm
    This fixes the gray background I'm seeing in k9s

--- dot-emacs-dot-d/lisp/feat-term.el
+++ dot-emacs-dot-d/lisp/feat-term.el
@@ -6,6 +6,7 @@
   (define-key evil-visual-state-map (kbd "M-V") #'aa/vterm-dwim)
   (define-key evil-emacs-state-map (kbd "M-V") #'aa/vterm-dwim)
   :config
+  (set-face-attribute 'vterm-color-black nil :foreground "#000000" :background "#000000")
   (add-hook 'vterm-mode-hook (lambda () (display-line-numbers-mode -1)))
   (defun aa/vterm-dwim ()
     "Switch to/from vterm buffer based on major mode"
