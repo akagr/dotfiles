188aa65e97b1fc9f4c07f4ca1c8c5f2cd0168293
Akash Agrawal <akagr@outlook.com>
2024-08-21 12:55:20 +0530

    Remove explicit lsp server config for already supported modes

--- dot-emacs-dot-d/lisp/feat-lsp.el
+++ dot-emacs-dot-d/lisp/feat-lsp.el
@@ -8,13 +8,11 @@
   :custom
   ;; Good default
   (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
-  (eglot-autoshutdown t);; Shutdown unused servers.
+  (eglot-autoshutdown t) ;; Shutdown unused servers.
   (eglot-report-progress nil) ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
   ;; Manual lsp servers
   :config
   (add-to-list 'eglot-server-programs
-               `(lua-mode . ("/opt/homebrew/bin/lua-language-server" "-lsp"))
-               `(terraform-mode . ("/opt/homebrew/bin/terraform-ls", "serve"))
-               `(typescript-mode . ("/Users/a.agrawal/.asdf/shims/typescript-language-server", "--stdio"))))
+               '(lua-mode . ("/opt/homebrew/bin/lua-language-server" "-lsp"))))
 
 (provide 'feat-lsp)
