2529e9eda2785a6a04332cfa3a8f07a8070c1bc4
Akash Agrawal <akagr@outlook.com>
2024-09-12 19:38:30 +0530

    Use gpt 4 omni by default

--- dot-emacs-dot-d/lisp/feat-ai.el
+++ dot-emacs-dot-d/lisp/feat-ai.el
@@ -1,3 +1,11 @@
-(use-package gptel)
+(use-package gptel
+  :custom
+  (gptel-model "gpt-4o")
+
+  :hook (gptel-mode . visual-line-mode)
+
+  :commands (gptel gptel-send gptel-mode gptel-menu))
+
+(global-set-key (kbd "C-c RET") #'gptel-send)
 
 (provide 'feat-ai)
