e4959390204184c9e16e187e948cd22e03b533af
Akash Agrawal <akagr@outlook.com>
2024-09-19 23:49:14 +0530

    Use lsp mode instead of eglot

--- dot-emacs-dot-d/init.el
+++ dot-emacs-dot-d/init.el
@@ -42,3 +42,15 @@
 (setq gc-cons-threshold (* 2 1000 1000))
 ;; Increase the amount of data which Emacs reads from the process
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(eglot edit-indirect)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- dot-emacs-dot-d/lisp/feat-lsp.el
+++ dot-emacs-dot-d/lisp/feat-lsp.el
@@ -1,18 +1,25 @@
-(use-package eglot
-  :ensure nil ;; Don't install eglot because it's now built-in
-  :hook ((c-mode c++-mode ;; Autostart lsp servers for a given mode
-                 lua-mode
-                 terraform-mode
-                 typescript-mode)
-         . eglot-ensure)
+;; (use-package eglot
+;;   :ensure nil ;; Don't install eglot because it's now built-in
+;;   :hook ((c-mode c++-mode ;; Autostart lsp servers for a given mode
+;;                  lua-mode
+;;                  terraform-mode
+;;                  typescript-mode)
+;;          . eglot-ensure)
+;;   :custom
+;;   ;; Good default
+;;   (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
+;;   (eglot-autoshutdown t) ;; Shutdown unused servers.
+;;   (eglot-report-progress nil) ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
+;;   ;; Manual lsp servers
+;;   :config
+;;   (add-to-list 'eglot-server-programs
+;;                '(lua-mode . ("/opt/homebrew/bin/lua-language-server" "-lsp"))))
+
+(use-package lsp-mode
   :custom
-  ;; Good default
-  (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
-  (eglot-autoshutdown t) ;; Shutdown unused servers.
-  (eglot-report-progress nil) ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
-  ;; Manual lsp servers
-  :config
-  (add-to-list 'eglot-server-programs
-               '(lua-mode . ("/opt/homebrew/bin/lua-language-server" "-lsp"))))
+  (lsp-completion-provider :none)
+  :hook ((terraform-mode . lsp-deferred))
+  :custom
+  (setq lsp-disabled-clients '(tfls)))
 
 (provide 'feat-lsp)
