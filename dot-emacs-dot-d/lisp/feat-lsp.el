8657c57f98472aafe16d25c951deec9cc19ea5dd
Akash Agrawal <akagr@outlook.com>
2024-12-06 10:59:10 +0530

    Switch back to eglot
    Didn't get what I needed from lsp-mode. It has same buggy
    integration for terraform mode

--- dot-emacs-dot-d/lisp/feat-lsp.el
+++ dot-emacs-dot-d/lisp/feat-lsp.el
@@ -1,25 +1,18 @@
-;; (use-package eglot
-;;   :ensure nil ;; Don't install eglot because it's now built-in
-;;   :hook ((c-mode c++-mode ;; Autostart lsp servers for a given mode
-;;                  lua-mode
-;;                  terraform-mode
-;;                  typescript-mode)
-;;          . eglot-ensure)
-;;   :custom
-;;   ;; Good default
-;;   (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
-;;   (eglot-autoshutdown t) ;; Shutdown unused servers.
-;;   (eglot-report-progress nil) ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
-;;   ;; Manual lsp servers
-;;   :config
-;;   (add-to-list 'eglot-server-programs
-;;                '(lua-mode . ("/opt/homebrew/bin/lua-language-server" "-lsp"))))
-
-(use-package lsp-mode
+(use-package eglot
+  :ensure nil ;; Don't install eglot because it's now built-in
+  :hook ((c-mode c++-mode ;; Autostart lsp servers for a given mode
+                 lua-mode
+                 terraform-mode
+                 typescript-mode)
+         . eglot-ensure)
   :custom
-  (lsp-completion-provider :none)
-  :hook ((terraform-mode . lsp-deferred))
-  :custom
-  (setq lsp-disabled-clients '(tfls)))
+  ;; Good default
+  (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
+  (eglot-autoshutdown t) ;; Shutdown unused servers.
+  (eglot-report-progress nil) ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
+  ;; Manual lsp servers
+  :config
+  (add-to-list 'eglot-server-programs
+               '(lua-mode . ("/opt/homebrew/bin/lua-language-server" "-lsp"))))
 
 (provide 'feat-lsp)
