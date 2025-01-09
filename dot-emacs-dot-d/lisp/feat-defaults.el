9ceaaa53e1ba7c0099ea01bd19381aa64d5f105e
Akash Agrawal <akagr@outlook.com>
2025-01-09 12:46:50 +0530

    Align comments

--- dot-emacs-dot-d/lisp/feat-defaults.el
+++ dot-emacs-dot-d/lisp/feat-defaults.el
@@ -31,10 +31,10 @@
     (tooltip-mode -1)
     (menu-bar-mode 1)) ;; keep menu bar enabled
 
-  (global-so-long-mode) ;; handles really long lines well
-  (column-number-mode) ;; show column number in mode line
+  (global-so-long-mode)     ;; handles really long lines well
+  (column-number-mode)      ;; show column number in mode line
   (global-auto-revert-mode) ;; reflect changes on disk to file automatically
-  (show-paren-mode) ;; show matching delimiters
+  (show-paren-mode)         ;; show matching delimiters
 
   (when (eq system-type 'darwin)
     (setq mac-option-key-is-meta nil
