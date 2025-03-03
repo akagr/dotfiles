da92984e0392eab1e93fbf25d58d4b94d4c73f66
Akash Agrawal <akagr@outlook.com>
2025-03-03 23:16:28 +0530

    Upgrade elpaca to 0.10

--- dot-emacs-dot-d/lisp/feat-elpaca.el
+++ dot-emacs-dot-d/lisp/feat-elpaca.el
@@ -1,7 +1,10 @@
 ;; Elpaca package manager
 ;; https://github.com/progfolio/elpaca
 
-(defvar elpaca-installer-version 0.9)
+;; To upgrade elpaca, following section needs to be replaced by
+;; the latest code snippet from the link above.
+;; ================================================================
+(defvar elpaca-installer-version 0.10)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -16,7 +19,7 @@
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
                   ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
@@ -39,6 +42,7 @@
     (load "./elpaca-autoloads")))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
+;; ================================================================
 
 ;; Install use-package support
 (elpaca elpaca-use-package
